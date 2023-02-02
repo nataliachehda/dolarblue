6017cd2bc37be0b71bcbe4d3cd7f4dcdceb17590
nataliachehda <116130811+nataliachehda@users.noreply.github.com>
2023-02-01 22:53:09 -0300

    subiendo

---
+++ scripts/javascript.js
@@ -0,0 +1,29 @@
+// comentario de linea
+/* comentario 
+de 
+bloque*/
+
+//${saludo} ${nombre}
+/*
+saludo = "hola";
+nombre = "nati";
+saludamiento = saludo + nombre;
+
+console.log (saludamiento)
+let nombre = prompt ("ingrese su nombre");
+console.log (nombre);
+*/
+
+//let nombre = prompt ("cual es tu nombre")
+//console.log ("hola " + nombre)
+/*
+let saludar = prompt ("decinos tu nombre")
+saludo = `hola ${saludar}`;
+console.log (saludo)
+*/
+
+let multiplica = parseInt (prompt ("qué monto en dolares queres transformar a pesos argentinos blue?"));
+let valor = 385;
+let moneda = " pesos argentinos";
+let pesificando = valor * multiplica;
+alert (pesificando + moneda)
