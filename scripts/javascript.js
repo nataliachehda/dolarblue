72a36fa7613ecdaa505d32e8c79684da8706c169
nataliachehda <116130811+nataliachehda@users.noreply.github.com>
2023-02-02 00:39:13 -0300

    corrección

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,4 +1,4 @@
-let multiplica = parseInt (prompt ("¿qué monto en dolares querés transformar a pesos argentinos blue?"));
+let multiplica = parseInt (prompt ("¿qué monto en dólares querés transformar a pesos argentinos blue?"));
 let valor = 385;
 let moneda = " pesos argentinos";
 let pesificando = valor * multiplica;
